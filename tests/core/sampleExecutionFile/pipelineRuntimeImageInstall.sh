d31f695c60bc96d3a2e0598b0410b4696a04f84f
JFrog Pipelines Beta Account <shipbeta@shippable.com>
2020-02-18 15:31:30 +0530

    Create pipelineRuntimeImageInstall.sh

---
+++ tests/core/sampleExecutionFile/pipelineRuntimeImageInstall.sh
@@ -0,0 +1,23 @@
+apt-get update
+  
+apt-get install sudo
+
+sudo apt install bash-completion
+
+sudo apt-get install sed
+
+sudo apt-get install coreutils
+
+sudo apt-get install -y jq
+
+sudo apt-get install -y gawk
+
+sudo apt install -y git
+
+sudo apt-get install gettext
+
+sudo apt install curl
+
+sudo apt-get install cl-base64
+
+sudo apt-get install openssl
